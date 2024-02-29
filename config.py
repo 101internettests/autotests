b6a535855021bda36c5cbddd397615e2727c4997
Katerina Mangileva <ms.katiysha@mail.ru>
2024-02-29 21:41:27 +0500

    Тестовое изменение

--- config.py
+++ config.py
@@ -4,5 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
-chat_id = int(os.getenv("CHAT_ID"))
+bot_token = os.getenv("BOT_TOKEN")
+chat_id_str = os.getenv("CHAT_ID")
+
+if bot_token is not None and chat_id_str is not None and chat_id_str.strip():
+    try:
+        bot = telebot.TeleBot(bot_token)
+        chat_id = int(chat_id_str)
+    except ValueError:
+        print("Error: Invalid values for BOT_TOKEN or CHAT_ID.")
+else:
+    print("Error: BOT_TOKEN or CHAT_ID environment variables are not set or empty.")
