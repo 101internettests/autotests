d524c95ac326e43a2b2193395b256624569394e4
Katerina Mangileva <ms.katiysha@mail.ru>
2024-03-01 00:39:28 +0500

    Тестовое изменение

--- conftest.py
+++ conftest.py
@@ -17,4 +17,4 @@ def driver():
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Заявки отправлены, отчет смотри здесь")
+    bot.send_message(chat_id, "Теговые тесты сделал, отчет тут - https://101internettests.github.io/autotests/")
