442cf772afa5b8f99218f474f060ae4f6323bf3a
zhzh25 <zhzh2592@mail.ru>
2024-03-06 18:02:31 +0300

    добавлены тесты проверки 404 страницы и несуществующих адресов

---
+++ tests/test_serch/test_serch_MOL.py
@@ -0,0 +1,9 @@
+from pages.serch.mol_page import CheckPage404
+import random
+
+
+class TestSerch:
+    def test_nonexistent_adress_mol(self, driver):
+        serch = CheckPage404(driver, "https://www.moskvaonline.ru/")
+        serch.open()
+        serch.check_nonexistent_adress_mol()
