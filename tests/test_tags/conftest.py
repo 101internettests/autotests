461b5a439afcc0448c1f7dd5efab41fe03f794d4
Katerina Mangileva <ms.katiysha@mail.ru>
2024-03-19 17:32:49 +0500

    Добавлены исправления для github actions

--- tests/test_tags/conftest.py
+++ tests/test_tags/conftest.py
@@ -2,6 +2,6 @@ import pytest
 from config import bot, chat_id
 
 
-@pytest.hookimpl(trylast=True)
-def pytest_sessionfinish(session, exitstatus):
-    bot.send_message(chat_id, "Теговые тесты сделал, отчет по ссылке - https://101internettests.github.io/autotests/")
+# @pytest.hookimpl(trylast=True)
+# def pytest_sessionfinish(session, exitstatus):
+#     bot.send_message(chat_id, "Теговые тесты сделал, отчет по ссылке - https://101internettests.github.io/autotests/")
