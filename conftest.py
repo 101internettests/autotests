12988fa1227bd2e2540b036d87a15af7e05a7cb6
101internettests <160827243+101internettests@users.noreply.github.com>
2024-06-27 10:24:00 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,51 +1,51 @@
-# import os
+import os
 import pytest
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from selenium import webdriver
-# from config import bot, chat_id
-# load_dotenv()
+from config import bot, chat_id
+load_dotenv()
 
 ]
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chome")
-    parser.addoption("--executor", action="store", default="172.24.16.1")
-
-
-@pytest.fixture
-def browser(request):
-    browser = request.confiq.getoption("--browser")
-    executor = request.confiq.getoption("--executor")
-
-    capabilities = {
-        "browserName": browser
-    }
-    driver = webdriver.Remote(
-        command_executor=f"http://{executor}:4444/wd/hub",
-        desired_capabilities=capabilities
-    )
-    request.addfinalizer(driver.quit)
-    return driver
+# def pytest_addoption(parser):
+#     parser.addoption("--browser", action="store", default="chome")
+#     parser.addoption("--executor", action="store", default="172.24.16.1")
 
 
 # @pytest.fixture
-# def driver():
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.add_argument('--incognito')
-#     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#     prefs = {
-#         "credentials_enable_service": False,
-#         "profile.password_manager_enabled": False
+# def browser(request):
+#     browser = request.confiq.getoption("--browser")
+#     executor = request.confiq.getoption("--executor")
+
+#     capabilities = {
+#         "browserName": browser
 #     }
-#     chrome_options.add_experimental_option("prefs", prefs)
-#     if os.getenv("HEADLESS") == "True":
-#         chrome_options.add_argument("--headless")
-#     driver = webdriver.Chrome(options=chrome_options)
-#     driver.set_window_size(1920, 1080)
-#     # driver.maximize_window()
-#     yield driver
-#     driver.quit()
+#     driver = webdriver.Remote(
+#         command_executor=f"http://{executor}:4444/wd/hub",
+#         desired_capabilities=capabilities
+#     )
+#     request.addfinalizer(driver.quit)
+#     return driver
 
 
-# @pytest.hookimpl(trylast=True)
-# def pytest_sessionfinish(session, exitstatus):
-#     bot.send_message(chat_id, "Все тесты сделал, отчет по ссылке - https://101internettests.github.io/autotests/")
+@pytest.fixture
+def driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    if os.getenv("HEADLESS") == "True":
+        chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1920, 1080)
+    # driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session, exitstatus):
+    bot.send_message(chat_id, "Все тесты сделал, отчет по ссылке - https://101internettests.github.io/autotests/")
